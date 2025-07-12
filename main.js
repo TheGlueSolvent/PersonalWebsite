b9fc781a2cdc54636a55f3eb7b2e97d68dc7dc54
TheGlueSolvent <epicgamer9131@gmail.com>
2025-07-12 14:28:01 -0400

    made a button

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-
+const invis = document.getElementById('invis');
+let opacity = 0;
+document.getElementById('button').addEventListener('click', () => {transparency(invis)});
+function transparency(target) {
+  target.style.opacity = opacity;
+  const intervalId = setInterval(() => {
+    if (opacity < 1) {
+      opacity += 0.01;
+      target.style.opacity = opacity;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 20); 
+}
